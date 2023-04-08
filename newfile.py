f8e92d5d66fc44633d9d041a0b2c404cd533f78a
VibhuDixit-2215001940 <122095294+VibhuDixit-2215001940@users.noreply.github.com>
2023-04-08 11:00:20 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,16 @@
+#Printing ANTI Diagonal Elements
+r=int(input("Enter the no. of rows: "))
+c=int(input("Enter the no. of columns: "))
+x=[]
+for i in range(r):
+	y=[]
+	for j in range(c):
+		y.append(input("Enter the value: "))
+	x.append(y)
+for i in range(r):
+	for j in range(c):
+		if(i+j==r-1):
+			print(x[i][j],end=' ')
+		else:
+			print( " ",end=' ')
+	print()
